3551d1315b6f22818e76530b6e24c57fa5b25d9f
Kyle Peeler <kylepeeler@gmail.com>
2018-04-22 23:31:17 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/huffmanEncoding.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/huffmanEncoding.dir/main.cpp.o"
+  "huffmanEncoding.pdb"
+  "huffmanEncoding"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/huffmanEncoding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
